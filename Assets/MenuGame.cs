cfcfaf5a7d2fb197b59b71dadf29f45dbf577a30
Kylian Aubry <kyaubry@c1r6p13.42lehavre.fr>
2024-02-10 21:51:37 +0100

    propre commit

---
+++ Assets/MenuGame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGame : MonoBehaviour
+{
+    public GameObject settingsWindow;
+
+    public GameObject canva;
+    
+    public void ResumeGame()
+    {
+        canva.SetActive(false);
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settingsWindow.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
